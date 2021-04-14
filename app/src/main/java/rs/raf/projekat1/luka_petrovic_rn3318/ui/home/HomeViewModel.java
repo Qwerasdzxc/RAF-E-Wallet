21af7c50af1f94030482c65a3da0a8f5208586cb
Luka Petrovic <petrovic.luka99@gmail.com>
2021-04-14 19:32:20 +0200

    Initial commit

---
+++ app/src/main/java/rs/raf/projekat1/luka_petrovic_rn3318/ui/home/HomeViewModel.java
@@ -0,0 +1,19 @@
+package rs.raf.projekat1.luka_petrovic_rn3318.ui.home;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class HomeViewModel extends ViewModel {
+
+    private MutableLiveData<String> mText;
+
+    public HomeViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is home fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
